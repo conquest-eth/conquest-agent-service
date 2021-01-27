9f313cde012e68bb3d68a19ba33850a4c453b776
Wighawag <wighawag@gmail.com>
2021-01-27 10:46:00 +0000

    cyan button

--- web/src/components/PanelButton.svelte
+++ web/src/components/PanelButton.svelte
@@ -1,12 +1,23 @@
 <script>
-  let class_names: string = '';
-  export {class_names as class};
   export let label: string;
 </script>
 
-<button
-  {label}
-  class="border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-gray-200 rounded px-4 py-2 {class_names}"
-  on:click>
-  <slot />
-</button>
+<div class="inline-block p-4 text-cyan-300 border-cyan-300">
+  <button {label} class="relative p-1 text-cyan-300 border-cyan-300">
+    <div
+      class="absolute left-0 -top-0 w-4 h-4 border-r-0 border-l-2 border-t-2 border-b-0 border-cyan-300" />
+    <div
+      class="absolute -left-0 -bottom-0 w-4 h-4 border-r-0 border-l-2 border-b-2 border-t-0 border-cyan-300" />
+    <div
+      class="absolute -right-0 -top-0 w-4 h-4 border-r-2 border-l-0 border-t-2 border-b-0 border-cyan-300" />
+    <div
+      class="absolute -right-0 -bottom-0 w-4 h-4 border-r-2 border-l-0 border-t-0 border-b-2 border-cyan-300" />
+    <div class="block relative border overflow-hidden border-cyan-600">
+      <div>
+        <div on:click class="px-4 py-2 relative">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </button>
+</div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {},
   plugins: [],
