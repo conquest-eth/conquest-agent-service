4956f820b1117cab95e7bd319255dcd9d7d285f8
Wighawag <wighawag@gmail.com>
2021-04-28 11:52:53 +0100

    help

--- web/src/routes/help.svelte
+++ web/src/routes/help.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import Banner from '$lib/components/Banner.svelte';
 </script>
 
-conquest.eth is a game of strategy and diplomacy where players compete to conquer the universe. Player can take control
-of empty planets by staking tokens on them. This activate their production of spaceships.
+<Banner>
+  <p>
+    conquest.eth is a game of strategy and diplomacy where players compete to conquer the universe. Player can take
+    control of empty planets by staking tokens on them. This activate their production of spaceships.
+  </p>
+</Banner>
