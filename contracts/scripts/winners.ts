749c71f7aa29de27b45619be608f30e12f240763
wighawag <wighawag@gmail.com>
2021-05-24 15:12:18 +0100

    winners

--- contracts/scripts/winners.ts
+++ contracts/scripts/winners.ts
@@ -1,22 +1,23 @@
 import 'dotenv/config';
-import {TheGraph} from '../utils/thegraph';
+import {TheGraph} from '../../subgraph/utils/thegraph';
 import {BigNumber} from '@ethersproject/bignumber';
+import fs from 'fs';
 
 const theGraph = new TheGraph(`https://api.thegraph.com/subgraphs/name/${process.env.SUBGRAPH_NAME}`);
 
 // query($blockNumber: Int! $first: Int! $lastId: ID! $id: ID!) {
 const queryString = `
 query($first: Int! $lastId: ID!) {
-    owners(first: $first where: {
-      totalStaked_gt: 0
-      id_gt: $lastId
-    }) {
-      id
-      currentStake
-      playTokenToWithdraw
-      playTokenBalance
-      playTokenGiven
-    }
+  owners(first: $first block: {number:4830319} where: {
+    totalStaked_gt: 0
+    id_gt: $lastId
+  }) {
+    id
+    currentStake
+    playTokenToWithdraw
+    playTokenBalance
+    playTokenGiven
+  }
 }
 `;
 
@@ -29,7 +30,7 @@ async function main() {
     playTokenToWithdraw: string;
     playTokenBalance: string;
     playTokenGiven: string;
-  }[] = await theGraph.query(queryString, 'owners', {});
+  }[] = await theGraph.query(queryString, {field: 'owners'});
   const winners = players
     .map((p) => {
       const currentStake = BigNumber.from(p.currentStake);
@@ -49,7 +50,40 @@ async function main() {
     })
     .sort((a, b) => b.score - a.score);
 
-  console.log(winners.slice(0, 18));
+  const top18 = winners.slice(0, 18);
+  console.log(top18);
+
+  console.log(top18.map((v) => v.id));
+
+  const winnersWithReward: {[id: string]: number} = {};
+  const tokenDistribution = [500, 200, 100, 50, 25, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5];
+  for (let i = 0; i < top18.length; i++) {
+    winnersWithReward[top18[i].id] = tokenDistribution[i];
+  }
+  console.log(winnersWithReward);
+
+  let winnersArray: {
+    address: string;
+    signedMessage?: string;
+    signature?: string;
+    numTokens: number;
+    numWCHI?: number;
+  }[] = [];
+  try {
+    winnersArray = JSON.parse(fs.readFileSync('winners.json').toString());
+  } catch (e) {}
+  for (const winner of Object.keys(winnersWithReward)) {
+    const found = winnersArray.findIndex((v) => v.address.toLowerCase() === winner);
+    if (found !== -1) {
+      winnersArray[found].numTokens = winnersWithReward[winner];
+    } else {
+      winnersArray.push({
+        address: winner,
+        numTokens: winnersWithReward[winner],
+      });
+    }
+  }
+  fs.writeFileSync('winners.json', JSON.stringify(winnersArray, null, 2));
 }
 
 main();
