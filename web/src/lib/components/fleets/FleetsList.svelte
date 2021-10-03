ffa1954121197f86c96e75e3ac4b11c73fc3ac40
Benja <Benja@MacBook-Pro-de-Benja.local>
2021-10-03 16:02:10 -0300

    events

--- web/src/lib/components/fleets/FleetsList.svelte
+++ web/src/lib/components/fleets/FleetsList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import {fleets} from '$lib/space/fleets';
   import type {Fleet} from '$lib/space/fleets';
+  import {clickOutside} from '$lib/utils/clickOutside';
   import {camera} from '$lib/map/camera';
 
   let isToggled = false;
@@ -22,21 +23,27 @@
   }
 </script>
 
-<div
-  class="absolute top-0 text-center right-2/4 inline-block w-64 bg-gray-900 bg-opacity-80 text-cyan-300 border-2 border-cyan-300 m-4 text-sm"
->
-  <button on:click={() => (isToggled = !isToggled)} class="text-white md:w-full">Fleets</button>
+<div class="flex-col" use:clickOutside on:click_outside={() => (isToggled = false)}>
+  <div
+    class="top-0 p-3 w-32 text-center relative bg-gray-900 bg-opacity-80 text-cyan-300 border-2 border-cyan-300 mt-4 text-sm"
+  >
+    <button on:click={() => (isToggled = !isToggled)} class="text-white md:w-full">My fleets</button>
+  </div>
   {#if isToggled}
-    {#if $fleets.length}
-      <ul class="overflow-auto max-h-32" style="cursor: pointer;">
-        {#each $fleets as fleet}
-          <li class="text-yellow-300" on:click={() => onFleetSelect(fleet)}>
-            {fleet.from.stats.name} to {fleet.to.stats.name}
-          </li>
-        {/each}
-      </ul>
-    {:else}
-      <h4 style="margin: 10px 0">No fleets yet</h4>
-    {/if}
+    <div
+      class="top-0 text-center absolute bg-gray-900 bg-opacity-80 text-cyan-300 border-2 p-3  border-cyan-300 mt-16 text-sm"
+    >
+      {#if $fleets.length}
+        <ul class="overflow-auto max-h-32" style="cursor: pointer;">
+          {#each $fleets as fleet}
+            <li class="text-yellow-300" on:click={() => onFleetSelect(fleet)}>
+              {fleet.from.stats.name} to {fleet.to.stats.name}
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <h4 style="margin: 10px 0">No fleets yet</h4>
+      {/if}
+    </div>
   {/if}
 </div>
