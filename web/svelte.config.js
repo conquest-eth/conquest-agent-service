0ef75ddd34526db3644dc2e32aa1d4b7084d6a11
wighawag <wighawag@gmail.com>
2021-11-09 12:22:42 +0000

    setup with non public sourcemap

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,16 +3,25 @@ import adapter_ipfs from 'sveltejs-adapter-ipfs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    sourceMap: true,
+  }),
 
   kit: {
     adapter: adapter_ipfs({
+      removeSourceMap: true,
+      copyBeforeSourceMapRemoval: 'release',
       removeBuiltInServiceWorkerRegistration: true,
       injectPagesInServiceWorker: true,
       injectDebugConsole: true,
     }),
     target: '#svelte',
     trailingSlash: 'ignore',
+    vite: {
+      build: {
+        sourcemap: true,
+      },
+    },
   },
 };
 
