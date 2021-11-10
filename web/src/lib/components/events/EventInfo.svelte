55011d5eb61ee8a54a0a17a37759b623477a3976
wighawag <wighawag@gmail.com>
2021-11-10 12:08:56 +0000

    fixes

--- web/src/lib/components/events/EventInfo.svelte
+++ web/src/lib/components/events/EventInfo.svelte
@@ -3,30 +3,50 @@
   import type {MyEvent} from '$lib/space/myevents';
   import {account} from '$lib/account/account';
   import {spaceInfo} from '$lib/space/spaceInfo';
-  import {fleetList} from '$lib/space/fleets';
-  import type {Fleet} from '$lib/space/fleets';
   import Button from '$lib/components/generic/PanelButton.svelte';
+  import {wallet} from '$lib/blockchain/wallet';
+  import Blockie from '../account/Blockie.svelte';
 
   export let event: MyEvent = null;
   export let isShow;
   let title;
-  let fleet: Fleet;
+
+  let fromYou = false;
+  let toYou = false;
   let gift = false;
+  let attackCaptured = false;
 
-  $: if (event.event.fleet) {
-    fleet = $fleetList.fleets.find((f) => f.resolution.id === event.event.transaction.id);
-    gift = true;
+  $: if (event.event) {
+    gift = event.event.gift;
+    attackCaptured = event.event.won;
+    if (event.event.owner.id === $wallet.address.toLowerCase()) {
+      fromYou = true;
+      if (event.event.gift) {
+        if (event.event.destinationOwner.id === event.event.owner.id) {
+          toYou = true;
+          title = 'Your spaceships landed!';
+        } else {
+          title = 'A friend send you some spaceships!';
+        }
+      } else if (event.event.won) {
+        title = 'You captured a planet!';
+      } else {
+        title = 'Your attack did not capture the planet!';
+      }
+    } else {
+      toYou = true;
+      if (event.event.gift) {
+        title = 'You received some spaceships';
+      } else if (event.event.won) {
+        title = 'You lost your planet!';
+      } else {
+        title = 'You got attacked but managed to keep your planet!';
+      }
+    }
+  } else {
+    title = 'Void event'; // TODO can it happen ?
   }
 
-  $: if (event?.event.won) {
-    title = 'You capture a new planet!';
-  } else if (gift && fleet.owner === event.event.owner.id) {
-    title = 'Your spaceships landed.';
-  } else if (gift) {
-    title = 'You received a gift ';
-  } else if (!gift && !event?.event.won) {
-    title = "You didn't capture this planet.";
-  }
   async function acknowledge() {
     await account.acknowledgeEvent(event);
     event = null;
@@ -43,21 +63,55 @@
       event = null;
     }}
   >
-    <ul class="mt-10 text-white">
-      {#if event?.event.won}
-        <li>Planet: {spaceInfo.getPlanetInfoViaId(event.event.planet.id).stats.name}</li>
-        <li>In flight fleet loss: {event.event.inFlightFleetLoss}</li>
-        <li>In flight planet loss: {event.event.inFlightPlanetLoss}</li>
-        <li>New spaceships: {event.event.newNumspaceships}</li>
-        <li>Planet loss: {event.event.planetLoss}</li>
-        <li>Fleet loss: {event.event.fleetLoss}</li>
-        <li>Quantity: {event.event.quantity}</li>
-      {:else if gift}
-        <li>From planet: {spaceInfo.getPlanetInfo(fleet.from.location.x, fleet.from.location.y).stats.name}</li>
-        <li>To planet: {spaceInfo.getPlanetInfoViaId(event.event.planet.id).stats.name}</li>
-        <li>Spaceships received: {event.event.quantity}</li>
+    {#if toYou && fromYou}
+      <!-- TODO add description-->
+    {:else if fromYou}
+      {#if gift}
+        Your {event.event.quantity - event.event.inFlightFleetLoss} spacesships arrived to <Blockie
+          class="w-6 h-6 inline-block"
+          address={event.event.destinationOwner.id}
+        />
+      {:else if attackCaptured}
+        You managed to capture a planet from <Blockie
+          class="w-6 h-6 inline-block"
+          address={event.event.destinationOwner.id}
+        />
+      {:else}
+        Your attack did not succeed but you killed {event.event.planetLoss} spaceships from <Blockie
+          class="w-6 h-6 inline-block"
+          address={event.event.destinationOwner.id}
+        />
       {/if}
-    </ul>
+    {:else if gift}
+      <Blockie class="w-6 h-6 inline-block" address={event.event.destinationOwner.id} /> sent you {event.event
+        .quantity - event.event.inFlightFleetLoss}
+      spacesships
+    {:else if attackCaptured}
+      <Blockie class="w-6 h-6 inline-block" address={event.event.destinationOwner.id} /> captured your planet!
+    {:else}
+      <Blockie class="w-6 h-6 inline-block" address={event.event.destinationOwner.id} /> attempted to capture your planet
+      but only managed to kill {event.event.planetLoss} spaceships.
+    {/if}
+    {#if event.event}
+      <ul class="mt-10 text-white">
+        <li>From planet: {spaceInfo.getPlanetInfoViaId(event.event.from.id).stats.name}</li>
+        <li>To planet: {spaceInfo.getPlanetInfoViaId(event.event.planet.id).stats.name}</li>
+        {#if event.event.inFlightFleetLoss > 0}
+          <!-- TODO add text to explain this-->
+          <li>In flight fleet loss: {event.event.inFlightFleetLoss}</li>
+        {/if}
+        {#if event.event.inFlightPlanetLoss > 0}
+          <!-- TODO add text to explain this-->
+          <li>In flight planet loss: {event.event.inFlightPlanetLoss}</li>
+        {/if}
+        <!-- <li>New spaceships: {event.event.newNumspaceships}</li> do not want to display that as it is stale -->
+        <!-- <li>Planet loss: {event.event.planetLoss}</li>
+        <li>Fleet loss: {event.event.fleetLoss}</li>
+        <li>Quantity: {event.event.quantity}</li> -->
+      </ul>
+    {:else}
+      Void Event <!-- TODO is that possible ?-->
+    {/if}
 
     <div class="text-center">
       <Button class="mt-4 text-center" label="Retry" on:click={acknowledge}>Ok</Button>
