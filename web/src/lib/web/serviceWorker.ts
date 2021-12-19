eb247e8a5d20480b030cbeee642aa002fdd17818
wighawag <wighawag@gmail.com>
2021-12-19 10:44:55 +0000

    make it easier to access service worker state

--- web/src/lib/web/serviceWorker.ts
+++ web/src/lib/web/serviceWorker.ts
@@ -1,16 +1,23 @@
-import {writable} from 'svelte/store';
+import {get, writable} from 'svelte/store';
 
 export type ServiceWorkerState = {
   registration?: ServiceWorkerRegistration;
   updateAvailable: boolean;
 };
 
-export const serviceWorker = writable<ServiceWorkerState>({registration: undefined, updateAvailable: false});
+const store = writable<ServiceWorkerState>({registration: undefined, updateAvailable: false});
+export const serviceWorker = {
+  ...store,
+  get registration(): ServiceWorkerRegistration | undefined {
+    return get(store).registration;
+  },
+  get updateAvailable(): boolean {
+    return get(store).updateAvailable;
+  },
+};
 
 // TODO remove
 if (typeof window !== 'undefined') {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).serviceWorker = serviceWorker;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (window as any).version = __VERSION__;
 }
