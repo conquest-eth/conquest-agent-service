47203e472afb7173e997210101f24cad1be54c27
wighawag <wighawag@gmail.com>
2022-03-21 21:52:36 +0000

    fix setup.cjs

--- web/.setup.cjs
+++ web/.setup.cjs
@@ -9,6 +9,28 @@ function copyFromDefault(p) {
   }
 }
 
+// from https://stackoverflow.com/questions/26827920/nodejs-recursively-list-files-in-directory/26828357#26828357
+function walk(directoryName, fnc) {
+  fs.readdir(directoryName, function (e, files) {
+    if (e) {
+      return;
+    }
+    files.forEach(function (file) {
+      var fullPath = directoryName + '/' + file;
+      fs.stat(fullPath, function (e, f) {
+        if (e) {
+          return;
+        }
+        if (f.isDirectory()) {
+          walk(fullPath, fnc);
+        } else {
+          fnc(fullPath);
+        }
+      });
+    });
+  });
+}
+
 [/*'.vscode/settings.json', */ '.env'].map(copyFromDefault);
 
 const overridesFolder = 'overrides';
