1b9112434fe9189729f2abb5ecc2688b2c814d86
wighawag <wighawag@gmail.com>
2022-03-22 11:53:02 +0000

    link on Coords

--- web/src/lib/components/utils/Coord.svelte
+++ web/src/lib/components/utils/Coord.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let location: string;
-  export let link: boolean = false;
+  // export let linkOnly: boolean = false;
 
   import {url} from '$lib/utils/url';
   import {coordFromXYObject, locationToXY} from 'conquest-eth-common';
@@ -11,19 +11,19 @@
   $: coordStr = coordFromXYObject(xy);
 </script>
 
-{#if link}
-  <Tooltip>
-    <div slot="tooltip">
-      <p class="m-1">
-        <a class="underline" href={url(`?x=${xy.x}&y=${xy.y}`)} style="cursor: pointer;">navigate to</a>
-      </p>
-    </div>
-    <Copiable text={`${coordStr}`}>
-      <span style="user-select: all; cursor: pointer;" class="border">{coordStr}</span>
-    </Copiable>
-  </Tooltip>
-{:else}
+<!-- {#if link} -->
+<Tooltip>
+  <div slot="tooltip">
+    <p class="m-1">
+      <a class="underline" href={url(`?x=${xy.x}&y=${xy.y}`)} style="cursor: pointer;">navigate to</a>
+    </p>
+  </div>
   <Copiable text={`${coordStr}`}>
     <span style="user-select: all; cursor: pointer;" class="border">{coordStr}</span>
   </Copiable>
-{/if}
+</Tooltip>
+<!-- {:else}
+  <Copiable text={`${coordStr}`}>
+    <span style="user-select: all; cursor: pointer;" class="border">{coordStr}</span>
+  </Copiable>
+{/if} -->
