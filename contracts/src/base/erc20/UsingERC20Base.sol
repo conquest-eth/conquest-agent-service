1dc825c8d32f31e68a1c81e61037836ce5f2dcb5
wighawag <wighawag@gmail.com>
2022-04-12 06:41:05 +0100

    ConquestToken add method to distribute different ETH values

--- contracts/src/base/erc20/UsingERC20Base.sol
+++ contracts/src/base/erc20/UsingERC20Base.sol
@@ -112,6 +112,27 @@ abstract contract UsingERC20Base is IERC20, ERC20BaseInternal {
         return true;
     }
 
+    function distributeVariousAmountsOfTokenAndETH(
+        address payable[] calldata tos,
+        uint256[] calldata tokenAmounts,
+        uint256[] calldata etherAmounts
+    ) external payable returns (bool) {
+        uint256 totalETHSent = 0;
+        require(tos.length == tokenAmounts.length, "TOKEN_NOT_SAME_LENGTH");
+        require(tos.length == etherAmounts.length, "ETH_NOT_SAME_LENGTH");
+        for (uint256 i = 0; i < tos.length; i++) {
+            if (tokenAmounts[i] != 0) {
+                _transfer(msg.sender, tos[i], tokenAmounts[i]);
+            }
+            if (etherAmounts[i] != 0) {
+                tos[i].transfer(etherAmounts[i]);
+                totalETHSent += etherAmounts[i];
+            }
+        }
+        require(msg.value == totalETHSent, "INVALID_MSG_VALUE");
+        return true;
+    }
+
     function transferAndCall(
         address to,
         uint256 amount,
